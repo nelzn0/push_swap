05976c97deee29095f7bd93eded84b6feb412c73
nelzn0 <nelsito.toto@gmail.com>
2026-05-19 20:04:48 +0100

    updated normalize

--- srcs/normalize.c
+++ srcs/normalize.c
@@ -6,9 +6,11 @@
 /*   By: nda-roch <nda-roch@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/19 18:39:45 by nda-roch          #+#    #+#             */
-/*   Updated: 2026/05/19 19:03:29 by nda-roch         ###   ########.fr       */
+/*   Updated: 2026/05/19 20:04:31 by nda-roch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include "push_swap.h"
 
 void	normalize(t_node **stack)
 {

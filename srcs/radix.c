848ed93801014b8d5bb8866a82ef009540c80516
nelzn0 <nelsito.toto@gmail.com>
2026-05-19 21:14:58 +0100

    fixed radix

--- srcs/radix.c
+++ srcs/radix.c
@@ -6,7 +6,7 @@
 /*   By: nda-roch <nda-roch@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/19 19:18:46 by nda-roch          #+#    #+#             */
-/*   Updated: 2026/05/19 20:38:38 by nda-roch         ###   ########.fr       */
+/*   Updated: 2026/05/19 21:09:49 by nda-roch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,12 +20,12 @@ void	radix(t_node **stack_a, t_node **stack_b)
 
 	i = 0;
 	n = stack_size(stack_a);
-	while ((1 << i) <= n)
+	while (((n - 1) >> i) != 0)
 	{
 		count = 0;
 		while (count < n)
 		{
-			if (((*stack_a)->value & (1 << i)) != 0)
+			if (((*stack_a)->value & (1 << i)) == 0)
 				pb(stack_a, stack_b);
 			else
 				ra(stack_a);

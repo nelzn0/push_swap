a88df8c98c6c46ab1c0cc30c42a58262ac56d541
nelzn0 <nelsito.toto@gmail.com>
2026-05-19 13:26:12 +0100

    added is_sorted

--- srcs/utils.c
+++ srcs/utils.c
@@ -6,7 +6,7 @@
 /*   By: nda-roch <nda-roch@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/18 19:46:14 by nda-roch          #+#    #+#             */
-/*   Updated: 2026/05/18 19:46:46 by nda-roch         ###   ########.fr       */
+/*   Updated: 2026/05/19 13:25:32 by nda-roch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,4 +33,24 @@ long	ft_atol(char *str)
 		str++;
 	}
 	return (sym * n);
+}
+
+int	is_sorted(t_node **stack)
+{
+	t_node	*pos0;
+	t_node	*pos1;
+
+	if (!stack || !*stack || !(*stack)->next)
+		return (1);
+	pos0 = *stack;
+	while (pos0)
+	{
+		pos1 = pos0->next;
+		if (!pos1)
+			break ;
+		if (pos0->value > pos1->value)
+			return (0);
+		pos0 = pos0->next;
+	}
+	return (1);
 }

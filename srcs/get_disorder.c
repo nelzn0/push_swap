956b4f68f63523a16732ff52189a9816a295ae11
nelzn0 <nelsito.toto@gmail.com>
2026-05-21 18:33:04 +0100

    added get_disorder.c

---
+++ srcs/get_disorder.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_disorder.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nda-roch <nda-roch@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/05/21 18:32:47 by nda-roch          #+#    #+#             */
+/*   Updated: 2026/05/21 18:32:53 by nda-roch         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+float	get_disorder(t_node **stack)
+{
+	size_t	mistakes;
+	size_t	pairs;
+	t_node	*pair_pos0;
+	t_node	*pair_pos1;
+
+	if (!stack || !*stack || !(*stack)->next)
+		return (0.0f);
+	mistakes = 0;
+	pairs = 0;
+	pair_pos0 = *stack;
+	while (pair_pos0 != NULL)
+	{
+		pair_pos1 = pair_pos0->next;
+		while (pair_pos1 != NULL)
+		{
+			pairs++;
+			if (pair_pos0->value > pair_pos1->value)
+				mistakes++;
+			pair_pos1 = pair_pos1->next;
+		}
+		pair_pos0 = pair_pos0->next;
+	}
+	return ((float)mistakes / pairs);
+}

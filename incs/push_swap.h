67cf95c2adabe6b1b790ca5a3290d216847d3a56
nelzn0 <nelsito.toto@gmail.com>
2026-05-21 21:33:46 +0100

    updated header

--- incs/push_swap.h
+++ incs/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: nda-roch <nda-roch@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/12 17:36:40 by nda-roch          #+#    #+#             */
-/*   Updated: 2026/05/21 21:19:50 by nda-roch         ###   ########.fr       */
+/*   Updated: 2026/05/21 21:33:35 by nda-roch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -64,7 +64,6 @@ void	normalize(t_node **stack);
 
 // utils
 long	ft_atol(char *str);
-int		is_valid_int(char *str);
 int		is_sorted(t_node **stack);
 void	free_stack(t_node **stack);
 int		stack_size(t_node *stack);

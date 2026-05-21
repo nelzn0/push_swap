77ac6e8d40788e0aea1f850311d094d4a43ef1c3
nelzn0 <nelsito.toto@gmail.com>
2026-05-21 18:27:24 +0100

    added chunk sort

---
+++ srcs/chunk_sort.c
@@ -0,0 +1,79 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   chunk_sort.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nda-roch <nda-roch@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/05/21 17:29:51 by nda-roch          #+#    #+#             */
+/*   Updated: 2026/05/21 18:18:46 by nda-roch         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+static void	push_to_b(t_node **stack_a, t_node **stack_b, int chunk_size)
+{
+	int	i;
+
+	i = 0;
+	while (*stack_a)
+	{
+		if ((*stack_a)->value <= i)
+		{
+			pb(stack_a, stack_b);
+			rb(stack_b);
+			i++;
+		}
+		else if ((*stack_a)->value <= i + chunk_size)
+		{
+			pb(stack_a, stack_b);
+			i++;
+		}
+		else
+			ra(stack_a);
+	}
+}
+
+static void	push_to_a(t_node **stack_a, t_node **stack_b)
+{
+	int	max_pos;
+	int	size_b;
+
+	while (*stack_b)
+	{
+		max_pos = get_max_pos(*stack_b);
+		size_b = stack_size(*stack_b);
+		if (max_pos <= size_b / 2)
+		{
+			while (max_pos)
+			{
+				rb(stack_b);
+				max_pos--;
+			}
+		}
+		else
+		{
+			while (max_pos < size_b)
+			{
+				rrb(stack_b);
+				max_pos++;
+			}
+		}
+		pa(stack_a, stack_b);
+	}
+}
+
+void	chunk_sort(t_node **stack_a, t_node **stack_b)
+{
+	int	size;
+	int	chunk_size;
+
+	size = stack_size(*stack_a);
+	if (size <= 100)
+		chunk_size = 15;
+	else
+		chunk_size = 35;
+	push_to_b(stack_a, stack_b, chunk_size);
+	push_to_a(stack_a, stack_b);
+}

f06224282cc035e4a087586ffb3fb23398412623
Nelson Da Rocha Dias <nda-roch@c1r10s6.42porto.com>
2026-05-28 14:17:56 +0100

    fixed push

--- srcs/push.c
+++ srcs/push.c
@@ -6,7 +6,7 @@
 /*   By: nda-roch <nda-roch@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/18 16:08:13 by nda-roch          #+#    #+#             */
-/*   Updated: 2026/05/28 13:55:22 by nda-roch         ###   ########.fr       */
+/*   Updated: 2026/05/28 14:14:50 by nda-roch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,14 @@
 
 void	pa(t_node **stack_a, t_node **stack_b, t_bench *bench)
 {
-	push(stack_b, stack_a);
+	push(stack_b, stack_a, bench);
 	ft_printf("pa\n");
 	bench->pa++;
 }
 
 void	pb(t_node **stack_a, t_node **stack_b, t_bench *bench)
 {
-	push(stack_a, stack_b);
+	push(stack_a, stack_b, bench);
 	ft_printf("pb\n");
 	bench->pb++;
 }
